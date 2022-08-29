91044e805d7921c2d997d4983dffc3b2da4ae9e4
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-08-29 07:10:39 -0700

    #7 : Footer update

--- src/components/CalendarPage/CalendarPage.jsx
+++ src/components/CalendarPage/CalendarPage.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 // import PropTypes from "prop-types";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import styles from "./CalendarPage.module.css";
-import DateController from "./../../package/DateController/DateController";
+import styles from './CalendarPage.module.css';
+import DateController from './../../package/DateController/DateController';
 
 const CalendarPage = (props) => {
   let navigate = useNavigate();
@@ -28,71 +28,86 @@ const CalendarPage = (props) => {
 
   return (
     <div className={styles.wrapper}>
-      <p align="center">
-        <button onClick={prev} children={"<"} />
-        {` ${year} `}
-        <button onClick={next} children={">"} />
-      </p>
-      <h2 align="center">
-        <font
-          style={{ backgroundColor: "rgba(100, 149, 237, 0.2)" }}
-          children="Верхнии"
-        />
-        {" и "}
-        <font
-          style={{ backgroundColor: "rgba(255, 207, 64, 0.3)" }}
-          children="нижнии"
-        />
-        {` недели за ${year}-${DateController.getNextYear(year)} гг.`}
-      </h2>
+      <div className={styles.content}>
+        <p align="center">
+          <button
+            onClick={prev}
+            title="Просмотреть календарь за прошлый год"
+            children={'<--'}
+          />
+          {` ${year} `}
+          <button
+            onClick={next}
+            title="Просмотреть календарь на следующий год"
+            children={'-->'}
+          />
+        </p>
+        <h2 align="center">
+          <font
+            style={{ backgroundColor: 'rgba(100, 149, 237, 0.2)' }}
+            children="Верхнии"
+          />
+          {' и '}
+          <font
+            style={{ backgroundColor: 'rgba(255, 207, 64, 0.3)' }}
+            children="нижнии"
+          />
+          {` недели за ${year}-${DateController.getNextYear(year)} гг.`}
+        </h2>
 
-      <ul className={styles.array}>
-        {calendar.map((el, i) => {
-          const { month, dates } = el;
-          const month_string = DateController.getStringMonth(month);
-          return (
-            <li key={i}>
-              <div className={styles.month_title}>{month_string}</div>
-              <ul className={styles.days_block}>
-                {["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"].map((element) => {
-                  return <li key={element}>{element}</li>;
-                })}
-              </ul>
-              <ul className={styles.month_block}>
-                {dates.map((element, i) => {
-                  const { date, isThisMonth } = element;
-                  const d = new Date(date);
-                  const weekend_type = DateController.getWeekType(d);
-                  const d_d = d.getDate();
-                  return (
-                    <li
-                      key={i}
-                      attribute_is_this_month={isThisMonth ? "true" : "false"}
-                      attribute_weekend_type={weekend_type}
-                      children={d_d}
-                    />
-                  );
-                })}
-              </ul>
-            </li>
-          );
-        })}
-      </ul>
+        <ul className={styles.array}>
+          {calendar.map((el, i) => {
+            const { month, dates } = el;
+            const month_string = DateController.getStringMonth(month);
+            return (
+              <li key={i}>
+                <div className={styles.month_title}>{month_string}</div>
+                <ul className={styles.days_block}>
+                  {['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].map((element) => {
+                    return <li key={element}>{element}</li>;
+                  })}
+                </ul>
+                <ul className={styles.month_block}>
+                  {dates.map((element, i) => {
+                    const { date, isThisMonth } = element;
+                    const d = new Date(date);
+                    const weekend_type = DateController.getWeekType(d);
+                    const d_d = d.getDate();
+                    return (
+                      <li
+                        key={i}
+                        attribute_is_this_month={isThisMonth ? 'true' : 'false'}
+                        attribute_weekend_type={weekend_type}
+                        children={d_d}
+                      />
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+        <p>
+          Верхняя неделя начинается с первого сентября. Когда заканчивается I'ый
+          семестр и начинается II'ой семестр, то отчёт продолжается с 1-го
+          сентября.
+        </p>
+        <p>
+          Сделал подсветку верхних и нижних недель. Так как июль-август
+          каникулы, то не подсвечивал.
+        </p>
+      </div>
       <footer className={styles.footer}>
-        Made with React and love by group PS-4
+        <p>Написано на React'e с любовью от группы ПО-4</p>
+        <p>
+          Ставь звезду, клонируй и редактируй код на GitHub:{' '}
+          <a
+            href="https://github.com/Pavel-Innokentevich-Galanin/what-univer-weekend"
+            title="Открыть репозиторий с кодом"
+            children={'Pavel-Innokentevich-Galanin/what-univer-weekend'}
+          />
+        </p>
       </footer>
-      <footer className={styles.footer_author}>
-        Pavel Innokentevich Galanin
-      </footer>
-      <p>
-        Верхняя неделя начинается с первого сентября. Когда заканчивается I'ый
-        семестр и начинается II'ой семестр, то отчёт продолжается с 1-го
-        сентября.
-      </p>
-      <p>
-        Сделал подсветку верхних и нижних недель. Так как июль-август каникулы,
-        то не подсвечивал.
-      </p>
     </div>
   );
 };
